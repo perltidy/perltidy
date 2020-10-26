cac55aaa38659598d7f4e5ab019cbcc267ed0a03
Steve Hancock <perltidy@users.sourceforge.net>
2020-10-26 06:53:55 -0700

    fixed typo in error message

--- lib/Perl/Tidy/Tokenizer.pm
+++ lib/Perl/Tidy/Tokenizer.pm
@@ -2478,7 +2478,7 @@ sub prepare_for_a_new_file {
                         complain("Long here-target: '$truncated' ...\n");
                     }
                     elsif ( !$here_doc_target ) {
-                        warning('Use of bare << to mean <<"" is deprecated\n')
+                        warning('Use of bare << to mean <<"" is deprecated'."\n")
                           unless ($here_quote_character);
                     }
                     elsif ( $here_doc_target !~ /^[A-Z_]\w+$/ ) {
